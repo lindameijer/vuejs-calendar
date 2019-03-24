612864df3420f21dafbfdaefdd41fa0b6cb33031
Linda Meijer <linda.meijer@nedap.com>
2019-03-24 15:16:37 +0100

    Position event-form

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -1,9 +1,18 @@
 <template>
-  <div id="event-form" :class="{active: true}" :style="{top: '500px', left: '500px'}"></div>
+  <div id="event-form" :class="{active: true}" :style="{top: top, left: left}"></div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    top() {
+      return `${this.$store.state.eventFormPosY}px`;
+    },
+    left() {
+      return `${this.$store.state.eventFormPosX}px`;
+    }
+  }
+};
 </script>
 
 <style>
