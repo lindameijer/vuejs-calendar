06f32f237d4906c5ed797378a99463f30b197612
Linda Meijer <linda.meijer@nedap.com>
2019-03-24 13:00:20 +0100

    Add visual month selector

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <div id="header">
+      <div>
+        <h1>Vue.js CalendarDay</h1>
+      </div>
+      <div>
+        <current-month></current-month>
+      </div>
+    </div>
     <div id="day-bar">
       <div>Mon</div>
       <div>Tue</div>
@@ -19,6 +27,7 @@
 
 <script>
 import CalendarDay from "./CalendarDay.vue";
+import CurrentMonth from "./CurrentMonth.vue";
 
 export default {
   computed: {
@@ -76,7 +85,8 @@ export default {
     }
   },
   components: {
-    CalendarDay
+    CalendarDay,
+    CurrentMonth
   }
 };
 </script>
---
+++ src/components/CurrentMonth.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div>{{ formattedDate }}</div>
+    <button @click="dec">-</button>
+    <button @click="inc">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    month() {
+      return this.$store.state.currentMonth;
+    },
+    year() {
+      return this.$store.state.currentYear;
+    },
+    formattedDate() {
+      return this.$moment(`${this.year}-${this.month}-1`, "YYYY-M-D").format(
+        "MMMM YYYY"
+      );
+    }
+  },
+  methods: {
+    dec() {
+      console.log("dec");
+    },
+    inc() {
+      console.log("inc");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
