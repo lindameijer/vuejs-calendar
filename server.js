6363bc9117df27c415e2420264c0ff2a473a04a1
Linda Meijer <linda.meijer@nedap.com>
2019-03-24 19:49:16 +0100

    Setup for sending events to server

--- server.js
+++ server.js
@@ -1,30 +1,37 @@
-require('dotenv').config({ silent: true });
+require('dotenv').config({ silent: true })
 
-const express = require('express');
-const app = express();
-const path = require('path');
-const fs = require('fs');
-const http = require('http');
+const express = require('express')
+const app = express()
+const path = require('path')
+const fs = require('fs')
+const http = require('http')
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  let template = fs.readFileSync(path.resolve('./index.html'), 'utf-8');
-  res.send(template);
+  let template = fs.readFileSync(path.resolve('./index.html'), 'utf-8')
+  res.send(template)
+})
 
-});
+let events = []
 
-const server = http.createServer(app);
+app.use(require('body-parser').json())
+app.post('/add_event', (req, res) => {
+  events.push(req.body)
+  res.sendStatus(200)
+})
+
+const server = http.createServer(app)
 
 if (process.env.NODE_ENV === 'development') {
-  const reload = require('reload');
-  const reloadServer = reload(app);
-  require('./webpack-dev-middleware').init(app);
+  const reload = require('reload')
+  const reloadServer = reload(app)
+  require('./webpack-dev-middleware').init(app)
 }
 
-server.listen(process.env.PORT, function () {
-  console.log(`Example app listening on port ${process.env.PORT}!`);
+server.listen(process.env.PORT, function() {
+  console.log(`Example app listening on port ${process.env.PORT}!`)
   if (process.env.NODE_ENV === 'development') {
-    require("opn")(`http://localhost:${process.env.PORT}`);
+    require('opn')(`http://localhost:${process.env.PORT}`)
   }
-});
+})
